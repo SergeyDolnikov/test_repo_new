d20c5ac32dc27e2c68a6647aea3135b6fc1d0403
Sergey Dolnikov <dolnikov90@mail.ru>
2022-11-01 16:50:41 +0300

    date "+%Y-%m-%d %H:%M"

---
+++ commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+datatime_now='date "+%Y-%m-%d %H:%M"'
+git config --global user.name "Sergey Dolnikov"
+git config --global user.email "dolnikov90@mail.ru"
+git add *
+git commit -m "$datatime_now"
+git branch -M main
+git remote add origin httrs://github.com/                        n.git
+git puch -u origin main
